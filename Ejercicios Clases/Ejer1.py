8b0f6f1634284112f5b84b6ecdd385dc2093ad39
guillevr777 <gs.villanueva@iesnervion.es>
2025-10-01 10:17:37 +0200

    Update Ejer1.py

---
+++ Ejercicios Clases/Ejer1.py
@@ -0,0 +1,43 @@
+class CuentaCorriente:
+    def __init__(self, dni, nombre, saldo):
+        self.dni = dni
+        self.nombre = nombre
+        self.saldo = saldo
+
+    def __init__(self, dni, saldo):
+        self.dni = dni
+        self.nombre = ""
+        self.saldo = saldo
+
+    def ingresarDinero(dinero):
+        self.saldo += dinero
+
+    def retirarDinero(dinero):
+        if self.saldo >= dinero:
+            self.saldo -= dinero
+        else:
+            print("No hay suficiente saldo")
+
+    def __str__(self):
+        cadena = "DNI: " + self.dni + " Nombre: " + self.nombre + " Saldo: " + str(self.saldo)
+        return cadena
+
+    def __eq__(self, objeto):
+        iguales = False
+        if self.dni == objeto.dni:
+            iguales = True
+        return iguales
+
+    def __lt__(self, objeto):
+        menor = False
+        if self.saldo < objeto.saldo:
+            menor = True
+        return menor
+    
+obj = CuentaCorriente("12345678A", "Juan Perez", 1000)
+obj2 = CuentaCorriente("87654321B", 500)
+
+obj.ingresarDinero(200)
+obj.retirarDinero(100)
+print(obj.__lt__(obj2))
+print(obj.__eq__(obj2))
